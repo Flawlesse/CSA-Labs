179da0fd3343aba269006053b5ed535a590dcfb6
Flawlesse <lavrenovichae@gmail.com>
2021-03-09 01:24:07 +0300

    Task 5 done. ll_cycle.cpp modified.

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -2,5 +2,20 @@
 
 bool ll_has_cycle(node *head) {
     /* Ваш код должен быть написан только внутри этой функции */
+    if (!head)
+    	return false;
+
+    node *hare = head, *turtle = head;
+    //do a flip!
+   	do
+   	{
+   		turtle = turtle->next;
+   		hare = hare->next;
+   		if (!turtle || !hare)
+   			return false;
+   		hare = hare->next;
+   		if (!hare)
+   			return false;
+   	} while (hare != turtle);
     return true;
 }
