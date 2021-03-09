f3fe3fd5f955998503b5d3ee413869d45a97543e
Flawlesse <lavrenovichae@gmail.com>
2021-03-10 00:35:03 +0300

    Task 8 done. lfsr.cpp modified.

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -2,6 +2,9 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t tmp = *reg;
+   	(*reg) >>= 1;
+   	uint16_t v = !!(tmp & 1) ^ !!(tmp & 4) ^ !!(tmp & 8) ^ !!(tmp & 32);
+   	(*reg) = ((*reg) & ~(1 << 15)) | (v << 15);
 }
 
